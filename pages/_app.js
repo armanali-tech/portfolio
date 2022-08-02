e1e2c55432de7039e4e7634c51b5b4659a2c62f9
Arman Ali <armanali.inno@gmail.com>
2022-08-02 14:52:01 +0530

    static file clean

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { Helmet } from "react-helmet";
 import { Provider } from "react-redux";
