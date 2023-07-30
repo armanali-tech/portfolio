4658e85928e0b9b50c1f202af76feecfef6b9be0
Arman Ali <armanali.inno@gmail.com>
2023-07-30 17:17:21 +0530

    Deployment bug fixed

--- components/custom/AboutMe.js
+++ components/custom/AboutMe.js
@@ -3,7 +3,7 @@ import React from "react";
 import Image from "next/image";
 import work from "../../assets/img/developer.webp";
 import { FaArrowCircleRight, FaCheckCircle } from "react-icons/fa";
-import Separator from "../resource/separator.js";
+import Separator from "../resource/Separator";
 
 const AboutMe = () => {
   // const router = useRouter();
